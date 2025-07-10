ba96d6cbb943d285a45df6331624511a402dcd7c
Black Beard <blackbeardpirates@tutamail.com>
2025-07-10 13:12:33 +0530

    Resolve versel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  server: {
+    // Ensuring Vite serves `index.html` for all routes
+    middlewareMode: true,
+  },
+});
